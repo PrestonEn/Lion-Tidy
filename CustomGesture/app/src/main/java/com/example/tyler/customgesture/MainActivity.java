3dfe91830370e0d8ed3ac292108c0e6d434863ac
DisneyLover <stace.v.14@gmail.com>
2015-11-20 03:23:00 -0500

    - Added graphics to the custom gesture example

--- CustomGesture/app/src/main/java/com/example/tyler/customgesture/MainActivity.java
+++ CustomGesture/app/src/main/java/com/example/tyler/customgesture/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tyler.customgesture;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.gesture.Gesture;
 import android.gesture.GestureLibraries;
 import android.gesture.GestureLibrary;
@@ -10,12 +11,14 @@ import android.gesture.GestureOverlayView;
 import android.gesture.GestureOverlayView.OnGesturePerformedListener;
 import android.gesture.Prediction;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnGesturePerformedListener {
     private GestureLibrary gestureLib;
-
+    private ImageView imgView;
 
     /** Called when the activity is first created. */
 
@@ -35,12 +38,23 @@ public class MainActivity extends Activity implements OnGesturePerformedListener
 
     @Override
     public void onGesturePerformed(GestureOverlayView overlay, Gesture gesture) {
+        imgView =(ImageView)findViewById(R.id.imageViewEarl);
+
         ArrayList<Prediction> predictions = gestureLib.recognize(gesture);
         for (Prediction prediction : predictions) {
-            if (prediction.score > 1.0) {
-                Toast.makeText(this, prediction.name, Toast.LENGTH_SHORT)
-                        .show();
+
+            if (prediction.score > 5.0) {
+                imgView.setImageResource(R.drawable.happy_earl);
             }
+            else{
+                imgView.setImageResource(R.drawable.sad_earl);
+            }
+
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    imgView.setImageResource(0);
+                }
+            }, 2000);
         }
     }
 }
